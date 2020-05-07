6ea774666f1ecc229ae03b14ce0fd2b3a4ea0a89
treem5e <m-sju-ke13@softbank.ne.jp>
2020-05-07 12:41:31 +0900

    Update index.vue

--- pages/index.vue
+++ pages/index.vue
@@ -8,15 +8,11 @@
       <v-col id="profile" class="text-center">
         <v-card class="profile">
           <h1 class="colum-title font-italic font-weight-bold">Profile</h1>
-          <h4 class="colum-title-ja font-italic font-weight-bold">
-            自己紹介
-          </h4>
+          <h4 class="colum-title-ja font-italic font-weight-bold">自己紹介</h4>
           <div class="profile-content">
             <v-list shaped>
               <v-list-item v-for="(list, i) in profileLists" :key="i">
-                <v-list-item-content class="text-sm-left">
-                  {{ list.value }}
-                </v-list-item-content>
+                <v-list-item-content class="text-sm-left">{{ list.value }}</v-list-item-content>
               </v-list-item>
             </v-list>
           </div>
@@ -24,19 +20,13 @@
       </v-col>
       <v-col id="currentWork" class="text-center" cols="12">
         <v-card class="currentWork">
-          <h1 class="colum-title font-italic font-weight-bold">
-            Current work content
-          </h1>
-          <h4 class="colum-title-ja font-italic font-weight-bold">
-            現在の仕事内容
-          </h4>
+          <h1 class="colum-title font-italic font-weight-bold">Current work content</h1>
+          <h4 class="colum-title-ja font-italic font-weight-bold">現在の仕事内容</h4>
           <div class="currentWork-content">
             <v-row cols="12">
               <v-col cols="6">
                 <h3 class="content-para">要件定義 ～ 詳細設計</h3>
-                <p>
-                  新規機能の追加や仕様変更の要件を任されたときには要件の洗い出しを行ったり、どの実装方法であれば要件を満たせるかを考えて仕様検討案の作成を行ったりしています。
-                </p>
+                <p>新規機能の追加や仕様変更の要件を任されたときには要件の洗い出しを行ったり、どの実装方法であれば要件を満たせるかを考えて仕様検討案の作成を行ったりしています。</p>
               </v-col>
               <v-col cols="6">
                 <h3 class="content-para">コーディング</h3>
@@ -52,9 +42,7 @@
                   リリース 1
                   ヶ月前くらいから所属しているプロジェクトの結合テストを行っています。
                 </p>
-                <p>
-                  この間に自分が担当した機能で不具合が発見された場合には不具合修正を行うこともあります。
-                </p>
+                <p>この間に自分が担当した機能で不具合が発見された場合には不具合修正を行うこともあります。</p>
               </v-col>
             </v-row>
           </div>
@@ -79,10 +67,7 @@
                 <p class="text-center headline">{{ COLUM_FW_LIB_TITLE }}</p>
                 <p />
                 <v-list shaped>
-                  <v-list-item
-                    v-for="(fw_lib, i) in framework_library"
-                    :key="i"
-                  >
+                  <v-list-item v-for="(fw_lib, i) in framework_library" :key="i">
                     <v-list-item-content>{{ fw_lib.name }}</v-list-item-content>
                   </v-list-item>
                 </v-list>
@@ -118,30 +103,29 @@
               <v-col class="content-style" cols="6">
                 <v-list-item-content>
                   <h3>ポートフォリオ</h3>
-                  <v-card-subtitle
-                    >Nuxt.js, Node.js, Vuetify.js</v-card-subtitle
-                  >
+                  <v-card-subtitle>Nuxt.js, Node.js, Vuetify.js</v-card-subtitle>
                   <a href="https://github.com/m5e2/Self-Introduction">Github</a>
                 </v-list-item-content>
               </v-col>
               <v-col class="content-style" cols="6">
                 <v-list-item-content>
                   <h3>イベント時出席者登録アプリ</h3>
-                  <v-card-subtitle
-                    >Java, Gradle, Thymeleaf, nginx, H2DB</v-card-subtitle
-                  >
-                  <a href="https://github.com/m5e/Acceptance-confirmation"
-                    >Github</a
-                  >
+                  <v-card-subtitle>Java, Gradle, Thymeleaf, nginx, H2DB</v-card-subtitle>
+                  <a href="https://github.com/m5e/Acceptance-confirmation">Github</a>
                 </v-list-item-content>
               </v-col>
               <v-col class="content-style" cols="6">
                 <v-list-item-content>
                   <h3>ルーレット</h3>
                   <v-card-subtitle>jQuery</v-card-subtitle>
-                  <a href="https://github.com/m5e/Practice-Roulette-Machine"
-                    >Github</a
-                  >
+                  <a href="https://github.com/m5e/Practice-Roulette-Machine">Github</a>
+                </v-list-item-content>
+              </v-col>
+              <v-col class="content-style" cols="6">
+                <v-list-item-content>
+                  <h3>スクレイピングを使ってブラウザからログイン</h3>
+                  <v-card-subtitle>Node.js, Puppeteer</v-card-subtitle>
+                  <a href="https://github.com/m5e/login_site">Github</a>
                 </v-list-item-content>
               </v-col>
               <v-col class="content-style" cols="6">
@@ -149,9 +133,7 @@
                   <h3>定期外交通費申請アプリ</h3>
                   <h3>( 製作途中 )</h3>
                   <v-card-subtitle>Nuxt.js, Node.js, bulma</v-card-subtitle>
-                  <a href="https://github.com/m5e/transportation-expenses"
-                    >Github</a
-                  >
+                  <a href="https://github.com/m5e/transportation-expenses">Github</a>
                 </v-list-item-content>
               </v-col>
             </v-row>
@@ -177,15 +159,15 @@ export default {
         { value: "フロントエンドエンジニア歴： 2 年 3 か月" },
         {
           value:
-            "経歴： 大学卒業後、物流系の会社に入社し、フォークリフトに乗りつつ1年ほど倉庫内業務を行っていました。その後、現職のSES企業に入社し、現在はフロントエンドエンジニアとして開発支援作業を行っています。",
-        },
+            "経歴： 大学卒業後、物流系の会社に入社し、フォークリフトに乗りつつ1年ほど倉庫内業務を行っていました。その後、現職のSES企業に入社し、現在はフロントエンドエンジニアとして開発支援作業を行っています。"
+        }
       ],
       Languages: [
         { name: "HTML" },
         { name: "CSS ( CSS, Less )" },
         { name: "Javascript" },
         { name: "Java" },
-        { name: "bash" },
+        { name: "bash" }
       ],
       framework_library: [
         { name: "jQuery" },
@@ -195,13 +177,13 @@ export default {
         { name: "Bootstrap" },
         { name: "Vuetify.js" },
         { name: "bulma" },
-        { name: "SpringBoot" },
+        { name: "SpringBoot" }
       ],
       db: [
         { name: "MySQL" },
         { name: "PostgreSQL" },
         { name: "H2 Database" },
-        { name: "Oracle" },
+        { name: "Oracle" }
       ],
       others: [
         { name: "Eclipse" },
@@ -215,8 +197,8 @@ export default {
         { name: "trac" },
         { name: "jenkins" },
         { name: "Apache" },
-        { name: "AWS" },
-      ],
+        { name: "AWS" }
+      ]
     };
   },
   mounted() {
@@ -224,7 +206,7 @@ export default {
   },
   methods: {
     handleScroll() {
-      const observer = new IntersectionObserver((entries) => {
+      const observer = new IntersectionObserver(entries => {
         for (const e of entries) {
           if (e.isIntersecting) e.target.classList.add("scrollin");
         }
@@ -234,7 +216,7 @@ export default {
       observer.observe(this.$el.querySelector(".currentWork-content"));
       observer.observe(this.$el.querySelector(".skill-content"));
       observer.observe(this.$el.querySelector(".works-content"));
-    },
+    }
   },
   created() {
     if (process.browser) window.addEventListener("scroll", this.handleScroll);
@@ -242,7 +224,7 @@ export default {
   destroyed() {
     if (process.browser)
       window.removeEventListener("scroll", this.handleScroll);
-  },
+  }
 };
 </script>
 <style scoped>
